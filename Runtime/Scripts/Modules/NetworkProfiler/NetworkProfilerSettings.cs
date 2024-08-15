be2bf364b67514f1778e528384aeb8e27836aa06
jKnepel <knepel.julian@tutanota.com>
2024-08-15 19:13:14 +0200

    chore: added descriptions to network profiler settings

--- Runtime/Scripts/Modules/NetworkProfiler/NetworkProfilerSettings.cs
+++ Runtime/Scripts/Modules/NetworkProfiler/NetworkProfilerSettings.cs
@@ -11,7 +11,6 @@ namespace jKnepel.ProteusNet.Modules.NetworkProfiler
     {
         public ProfilerAlignment Alignment = ProfilerAlignment.TopRight;
         public bool ShowNetworkManagerAPI = true;
-        public bool ShowSystemMetrics = true;
         public Color FontColor = new(.56f, .66f, .24f, 1);
         public string ProfilerFilePath = string.Empty;
         public string ClientProfileFileName = "clientNetworkStatistics.csv";
@@ -38,13 +37,12 @@ namespace jKnepel.ProteusNet.Modules.NetworkProfiler
             _areSettingsVisible = EditorGUILayout.Foldout(_areSettingsVisible, "Settings", true);
             if (_areSettingsVisible)
             {
-                EditorGUILayout.PropertyField(property.FindPropertyRelative("Alignment"), new GUIContent("Alignment"));
-                EditorGUILayout.PropertyField(property.FindPropertyRelative("ShowNetworkManagerAPI"), new GUIContent("Show NetworkManager API"));
-                EditorGUILayout.PropertyField(property.FindPropertyRelative("ShowSystemMetrics"), new GUIContent("Show System Metrics"));
-                EditorGUILayout.PropertyField(property.FindPropertyRelative("FontColor"), new GUIContent("Font Color"));
-                EditorGUILayout.PropertyField(property.FindPropertyRelative("ProfilerFilePath"), new GUIContent("Profiler Filepath"));
-                EditorGUILayout.PropertyField(property.FindPropertyRelative("ClientProfileFileName"), new GUIContent("Client Profile Filename"));
-                EditorGUILayout.PropertyField(property.FindPropertyRelative("ServerProfileFileName"), new GUIContent("Server Profile Filename"));
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("Alignment"), new GUIContent("Alignment", "Where to align the profiler GUI in the visible window."));
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("ShowNetworkManagerAPI"), new GUIContent("Show NetworkManager API", "Whether to show buttons for controlling the network manager in the profiler GUI."));
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("FontColor"), new GUIContent("Font Color", "The color used for the GUI font."));
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("ProfilerFilePath"), new GUIContent("Profiler Filepath", "The path to which network traffic statistics will be exported. If left empty, the default Unity persistent data path will be used."));
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("ClientProfileFileName"), new GUIContent("Client Filename", "The filename used for the client traffic statistics."));
+                EditorGUILayout.PropertyField(property.FindPropertyRelative("ServerProfileFileName"), new GUIContent("Server Filename", "The filename used for the server traffic statistics."));
             }
             EditorGUI.EndProperty();
         }
